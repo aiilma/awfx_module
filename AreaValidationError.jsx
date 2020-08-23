74aee11f5be20364b8e202484ac5e284453a48c7
vitalymo <vitaly.mortikov@mail.ru>
2020-08-23 17:34:05 +0300

    AreaValidationError is created

--- AreaValidationError.jsx
+++ AreaValidationError.jsx
@@ -1,8 +1,20 @@
-// function AreaValidationError(id, message) {
-//     this.id = id;
-//     this.message = message;
-// }
-//     AreaValidationError.prototype.toString = function() {
-//         return "id: " + this.id + "\n" + "message: " + this.message;
-//     }
-// throw new ValidationError(100, 'oops');
+function AreaValidationError(id, message) {
+    this.id = id;
+    this.message = message || "Undefined error.";
+}
+
+var AreaValidationErrorPrototype = {};
+AreaValidationErrorPrototype.prototype = Error.prototype;
+
+AreaValidationError.prototype = AreaValidationErrorPrototype;
+AreaValidationError.prototype.name = "AreaValidationError";
+AreaValidationError.prototype.constructor = AreaValidationError;
+AreaValidationError.prototype.toString = function() {
+    var res = '';
+
+    res += "name: " + this.name + "\n";
+    res += "id: " + this.id + "\n";
+    res += "message: " + this.message;
+
+    return res;
+}
